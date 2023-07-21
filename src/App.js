02ff0650370a782e6da59e1e1c265c3473b64dac
aria <mrruler1340@gmail.com>
2023-07-21 12:05:57 +0330

    build the base

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Header from './components/Header';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
+import createCache from '@emotion/cache';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const darkTheme = createTheme({
+  direction: 'rtl',
+  palette: {
+    mode: 'dark',
+    background:{
+      paper : '#133165'
+    }
+  },
+  // primary: ,
+});
+
+
+
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      {/* <CssBaseline /> */}
+    <Header />
+  </ThemeProvider>
+  );
+}
+
+export default App;
