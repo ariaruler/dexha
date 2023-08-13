ac71ae7135fc30d7ba6e17b8b72067b5215e0cbc
aria <mrruler1340@gmail.com>
2023-08-13 18:55:12 +0330

    14.6.1

--- src/components/PopUpTrade.jsx
+++ src/components/PopUpTrade.jsx
@@ -29,6 +29,7 @@ import TableRow from "@mui/material/TableRow";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import TradeBoared from "./TradeBoared";
 
+import QRcode from 'qrcode.react'
 
 const inputHieght = 54;
 const bigbuttonBorderRadius = "8px";
@@ -127,6 +128,99 @@ export default function PopUpTrade(props) {
             );
             break;
           case 1:
+            return (
+              <Dialog
+                sx={Dialogstyle}
+                maxWidth="xs"
+                fullWidth={true}
+                onClose={handleClose}
+                open={open === id}
+              >
+                <PopUpTitle
+                  header="تائید"
+                  previosStep={previosStep}
+                  handleClose={handleClose}
+                />
+              <TradeBoared />
+
+              
+                <DialogContent sx={{ padding: "2px 2em" }}>
+                  <Grid item xs={12}>
+                    <TableContainer>
+                      <Table>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell sx={{ padding: 0.5, border: "none" }}>
+                              منبع تبادل
+                            </TableCell>
+                            <TableCell
+                              sx={{ padding: 0.5, border: "none" }}
+                              align="right"
+                            >
+                              100%
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell sx={{ padding: 0.5, border: "none" }}>
+                              کارمزد تبادل
+                            </TableCell>
+                            <TableCell
+                              sx={{ padding: 0.5, border: "none" }}
+                              align="right"
+                            >
+                              0.09%
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell sx={{ padding: 0.5, border: "none" }}>
+                              کمترین میزان تبادل
+                            </TableCell>
+                            <TableCell
+                              sx={{ padding: 0.5, border: "none" }}
+                              align="right"
+                            >
+                              1865.3961 DAI
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell sx={{ padding: 0.5, border: "none" }}>
+                              فی شبکه
+                            </TableCell>
+                            <TableCell
+                              sx={{ padding: 0.5, border: "none" }}
+                              align="right"
+                            >
+                              $4.94
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Grid>
+                </DialogContent>
+
+                <DialogActions
+                  onClick={() => {
+                    setStep(step + 1);
+                  }}
+                  sx={{ backgroundColor: theme.palette.primary.main }}
+                >
+                  <Grid
+                    item
+                    sx={{
+                      margin: 1,
+                      display: "flex",
+                      justifyContent: " center ",
+                    }}
+                    xs={12}
+                  >
+                    مرحله بعد
+                  </Grid>
+                </DialogActions>
+              </Dialog>
+            );
+            break;
+          case 2:
             return (
               <Dialog
                 sx={Dialogstyle}
